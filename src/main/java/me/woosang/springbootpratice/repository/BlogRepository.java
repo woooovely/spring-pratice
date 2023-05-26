d513ea5bcd26caa1cba8655018a8d991324ad839
CHO Woosang <woosang3078@dsm.hs.kr>
2023-05-26 11:41:34 +0900

    feat: 리포지토리 추가

---
+++ src/main/java/me/woosang/springbootpratice/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.woosang.springbootpratice.repository;
+
+import me.woosang.springbootpratice.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
