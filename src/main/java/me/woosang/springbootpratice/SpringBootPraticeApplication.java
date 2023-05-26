6a50a2b2ab0e95116265d70bc7202f80e97d2325
CHO Woosang <woosang3078@dsm.hs.kr>
2023-05-26 10:51:03 +0900

    프로젝트 세팅

---
+++ src/main/java/me/woosang/springbootpratice/SpringBootPraticeApplication.java
@@ -0,0 +1,11 @@
+package me.woosang.springbootpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPraticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPraticeApplication.class, args);
+    }
+}
