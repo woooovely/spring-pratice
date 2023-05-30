347c6ebff1f66257083cd980702ced2c577d46c6
CHO Woosang <woosang3078@dsm.hs.kr>
2023-05-30 09:22:02 +0900

    feat: 컨트롤러 메소드 코드 추가

---
+++ src/main/java/me/woosang/springbootpratice/dto/ArticleResponse.java
@@ -0,0 +1,15 @@
+package me.woosang.springbootpratice.dto;
+
+import lombok.Getter;
+import me.woosang.springbootpratice.domain.Article;
+
+@Getter
+public class ArticleResponse {
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
