a892e4fe4f1e50f889e86d875f27d806bd5a2eb4
CHO Woosang <woosang3078@dsm.hs.kr>
2023-06-12 14:48:37 +0900

    docs: 컨트롤러 메소드 코드 추가

--- src/main/java/me/woosang/springbootpratice/controller/BlogApiController.java
+++ src/main/java/me/woosang/springbootpratice/controller/BlogApiController.java
@@ -7,10 +7,7 @@ import me.woosang.springbootpratice.dto.ArticleResponse;
 import me.woosang.springbootpratice.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -32,5 +29,12 @@ public class BlogApiController {
         List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok().body(new ArticleResponse(article));
     }
 }
