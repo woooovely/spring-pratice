8ab5eb368733f34f71253524e777896335170891
CHO Woosang <woosang3078@dsm.hs.kr>
2023-06-14 19:25:05 +0900

    docs: 글 수정 메소드 추가

--- src/main/java/me/woosang/springbootpratice/service/BlogService.java
+++ src/main/java/me/woosang/springbootpratice/service/BlogService.java
@@ -4,9 +4,11 @@ package me.woosang.springbootpratice.service;
 import lombok.RequiredArgsConstructor;
 import me.woosang.springbootpratice.domain.Article;
 import me.woosang.springbootpratice.dto.AddArticleRequest;
+import me.woosang.springbootpratice.dto.UpdateArticleRequest;
 import me.woosang.springbootpratice.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -28,5 +30,14 @@ public class BlogService {
 
     public void delete(long id) {
         blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
