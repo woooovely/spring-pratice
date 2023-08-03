fd0e4beb7917091dc4ca0301863fb279d2501975
woosang <woosang3078@hanmail.net>
2023-08-03 11:06:41 +0900

    docs: 엔티티 컬럼 업데이팅 어노테이션 추가

--- src/main/java/me/woosang/springbootpratice/SpringBootPraticeApplication.java
+++ src/main/java/me/woosang/springbootpratice/SpringBootPraticeApplication.java
@@ -2,7 +2,9 @@ package me.woosang.springbootpratice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootPraticeApplication {
     public static void main(String[] args) {
