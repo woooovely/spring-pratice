03586e7ce00f4e3d09b6b3af0ed4bffc3edd134f
woosang <woosang3078@hanmail.net>
2023-08-03 11:45:28 +0900

    feat: 컨트롤러 메소드 추가

--- src/main/java/me/woosang/springbootpratice/controller/BlogViewController.java
+++ src/main/java/me/woosang/springbootpratice/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.woosang.springbootpratice.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.woosang.springbootpratice.domain.Article;
 import me.woosang.springbootpratice.dto.ArticleListViewResponse;
+import me.woosang.springbootpratice.dto.ArticleViewResponse;
 import me.woosang.springbootpratice.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -21,5 +24,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
