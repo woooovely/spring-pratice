c6a863d449b6caeb924784fe242471b1d1f00f49
woosang <woosang3078@hanmail.net>
2023-08-04 09:48:39 +0900

    feat: 수정/생성 뷰 컨트롤러 추가

--- src/main/java/me/woosang/springbootpratice/controller/BlogViewController.java
+++ src/main/java/me/woosang/springbootpratice/controller/BlogViewController.java
@@ -37,6 +37,12 @@ public class BlogViewController {
 
     @GetMapping("/new-article")
     public String newArticle(@RequestParam(required = false) Long id, Model model) {
-
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
